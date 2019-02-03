df730af1dc1b4d48424703d117ec1fade318afd8
Richard Dallaway <richard@dallaway.com>
2019-02-03 18:13:48 +0000

    make generic of time type

--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ scalaVersion := "2.12.8"
 
 scalacOptions ++= Seq(
   "-deprecation",
-  "-encoding", "UTF-8",
+  "-encoding",
+  "UTF-8",
   "-unchecked",
   "-feature",
   "-language:implicitConversions",
@@ -17,7 +18,7 @@ scalacOptions ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "com.typesafe.slick" %% "slick"           % "3.3.0",
-  "ch.qos.logback"      % "logback-classic" % "1.2.3"
+  "com.typesafe.slick" %% "slick" % "3.3.0",
+  "com.chuusai" %% "shapeless" % "2.3.3",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
 )
-
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,30 +1,73 @@
 object Dump extends App {
 
-  case class Description(javaTypeName: String, sqlTypeName: String, sqlExampleLiteral: String)
+  import java.time._
+  import shapeless._
 
-  import slick.jdbc.JdbcType
-  def describe[T : JdbcType](exampleJvmValue: T): Description = {
-      val timeType = implicitly[JdbcType[T]]
-      Description(
-        exampleJvmValue.getClass().getName(),
-        timeType.sqlTypeName(None),
-        timeType.valueToSQLLiteral(exampleJvmValue),
-      )
-  }
-
-  import java.time.Instant
-  val example: Instant = Instant.now()
-
+  //For each of these profiles...
   val profiles =
     slick.jdbc.PostgresProfile ::
     slick.jdbc.DerbyProfile ::
     Nil
 
-  for (profile <- profiles) {
-      println(profile.getClass().getName().init)
-      import profile.api._
-      val description = describe(example)
-      println(description)
-    }
+  // ...I want to see the SQL Type and example SQL literal
+  // for each of these java.time types:
+  val examples = Instant.now() :: LocalDateTime.now() :: HNil
 
+  // To do that, I'll describe each example:
+  case class Description(
+    javaTypeName: String,
+    sqlTypeName: String,
+     sqlExampleLiteral: String
+   )
+
+  // For a type T, providing there's a JdbcType for it,
+  // we can describe it by calling a couple of methods that
+  // Slick provides:
+  import slick.jdbc.JdbcType
+  def describe[T: JdbcType](exampleJvmValue: T): Description = {
+    val timeType = implicitly[JdbcType[T]]
+    Description(
+      exampleJvmValue.getClass().getName(),
+      timeType.sqlTypeName(None),
+      timeType.valueToSQLLiteral(exampleJvmValue),
+    )
+  }
+
+  // Because we want to map over an HList (to keep the types
+  // of each example), we lift `describe` to a poly function:
+  object polyDescribe extends Poly1 {
+    implicit def default[T: JdbcType] = at[T](t => describe(t))
+  }
+
+  // E.g., the class slick.jdbc.PostgresProfile$ -> "PostgresProfile"
+  case class DbName(value: String)
+  def dbName(profile: slick.jdbc.JdbcProfile): DbName  =
+    DbName(profile.getClass().getName().init)
+
+  // Generate the descriptions:
+  val descriptions: Seq[(DbName, Seq[Description])] =
+    for (profile <- profiles) yield {
+      import profile.api._
+      val name = dbName(profile)
+      val timeDescriptions = examples.map(polyDescribe)
+      (name -> timeDescriptions.toList)
+  }
+
+  // Output as markdown:
+  println(asMarkdown(descriptions))
+
+  def asMarkdown(row: Description): String =
+    s"| `${row.javaTypeName}` | `${row.sqlTypeName}` | `${row.sqlExampleLiteral}` |"
+
+  def asMarkdown(dbDescriptions: Seq[(DbName, Seq[Description])]): String = {
+    dbDescriptions.map{ case (name, ds) =>
+      s"""# `${name.value}`
+
+| Java Type | SQL Type | Example SQL Literal |
+|-----------|----------|---------------------|
+${ds.map(asMarkdown).mkString("\n")}
+      
+      """
+  }.mkString("\n")
+  }
 }
